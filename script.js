c5da11fbaa1d99df501e07153e7d6346143eb05b
Priyanka <priyankagjune@gmail.com>
2024-12-09 17:02:20 -0700

    creating functions

--- script.js
+++ script.js
@@ -1,49 +1,67 @@
 let str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
 
-let tempString = str.split("\n");
-console.log(tempString);
-let row = tempString.length;
-console.log(row);
+let result = create2DArray(); // Caching the array in result for later use.
+console.log(result, " 2 d array creation");
+function create2DArray() {
+  //Part. 2 Expanding Functionality
+  let tempString = str.split("\n");
+  console.log(tempString);
+  let row = tempString.length;
+  console.log(row);
 
-let updateArray = tempString.map((x) => x.split(","));
+  let updateArray = tempString.map((x) => x.split(","));
+  //updatedArray is the 2 d array
 
-console.log(updateArray);
-let col = updateArray[0].length;
-console.log(col);
-//Part. 2
-let result = [];
-for (let i = 1; i < updateArray.length; i++) {
-  let obj = {};
-  for (let j = 0; j < col; j++) {
-    let x = updateArray[0][j];
-    //console.log(x);
-    obj[x.toLowerCase()] = updateArray[i][j];
-    // console.log(updateArray[i][j]);
-    // console.log(obj[x]);
+  console.log(updateArray);
+  let col = updateArray[0].length;
+  console.log(col);
+
+  //Part. 3 Transforming Data
+
+  let result = [];
+  for (let i = 1; i < updateArray.length; i++) {
+    let obj = {}; //Declaring an object to be pushed into the array
+    for (let j = 0; j < col; j++) {
+      let x = updateArray[0][j];
+
+      obj[x.toLowerCase()] = updateArray[i][j]; //
+    }
+    result.push(obj);
   }
-  result.push(obj);
+  //console.log(result);
+  return result;
 }
-console.log(result);
-//Part. 3
+
+//Part 4: Sorting and Manipulating Data
 
 //i.Remove the last element from the sorted array.
 
-console.log(result.pop());
+removeLastElement();
+
+function removeLastElement() {
+  console.log(result.pop());
+}
 
 //ii.Insert the following object at index 1:
 
-result.splice(1, 0, {
-  id: "48",
-  name: "Barry",
-  occupation: "Runner",
-  age: "25",
-});
+insertAtIndex();
 
+function insertAtIndex() {
+  result.splice(1, 0, {
+    id: "48",
+    name: "Barry",
+    occupation: "Runner",
+    age: "25",
+  });
+}
 //iii.Add the following object to the end of the array:
-
-result.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" });
-
 console.log(result, " Adding at the end");
+addToEnd();
+
+function addToEnd() {
+  result.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" });
+}
+
 //iv.Finally, use the values of each object within the array and the array’s length property to calculate the average age of the group. This calculation should be accomplished using a loop.
 let avgAge = 0;
 for (let i = 0; i < result.length; i++) {
@@ -55,6 +73,7 @@ console.log(avgAge / result.length);
 
 let objKeys = result.map((r) => Object.keys(r));
 console.log(objKeys);
+
 let tempKeys = objKeys.map((r) => r.join(","));
 
 let objVal = result.map((ele) => Object.values(ele));
@@ -65,7 +84,11 @@ console.log(tempVal[1] + "=========");
 let csv = tempKeys[0] + "\\n";
 
 for (let i = 0; i < tempVal.length - 1; i++) {
-  csv += tempVal[i] + "\\n";
+  if (i == tempVal.length - 2) {
+    csv += tempVal[i];
+  } else {
+    csv += tempVal[i] + "\\n";
+  }
 }
 
 let q = csv[csv.length - 2];
